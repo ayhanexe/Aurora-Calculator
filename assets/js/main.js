9bcde1069e6945d49becb3d3bd21ce8addd1731b
0x21106 <ayhan2015007@gmail.com>
2019-08-11 16:03:22 +0400

    Simple Calculator

---
+++ assets/js/main.js
@@ -0,0 +1,91 @@
+document.addEventListener('DOMContentLoaded', function() {
+    
+    console.log('%cCoded By 0x21106', 'background-color:black; padding:10px; font-family:Calibri; font-size:2.5em; font-weight:bold; color:#FF464C;')
+    console.log('%cCalculator Version 1.0.0', 'background-color:black; padding:10px; font-family:Calibri; font-size:2em; font-weight:bold; color:greenyellow;')
+    
+    let buttons,
+        screen,
+        screenValue = "0";
+        progressCount = 0;
+        dotCount = 0;
+    
+    
+    function init() {
+        buttons = document.querySelectorAll('.calculator-button')
+        screen = document.querySelector('#calculator-screen')
+        buttons.forEach(function(button) {
+            button.addEventListener('click', function() {
+                calculatorBtnClicked(this)
+                renderCalculatorScreen(screenValue)
+            })
+        })
+    }
+    init()
+    
+    function calculatorBtnClicked(button) {
+        let value = button.getAttribute('data-value')
+        if(!isNaN(value)) {
+            if(screenValue == '0') {
+                screenValue = Number(value)
+            } else {
+                screenValue = screenValue.toString() +  Number(value)
+            }
+        } else {
+            let lastChar = screenValue.toString().substring(screenValue.toString().length - 1)
+            if(value === 'del') {
+                if(screenValue.toString().length > 1) {
+                    screenValue = screenValue.toString().substring(0, screenValue.toString().length - 1)
+                } else {
+                    screenValue = '0'
+                }
+            }
+            if(value === 'proc') {
+                if(!isNaN(lastChar)) {
+                    screenValue = eval(screenValue);
+                }
+            }
+            else if(value === '+' || value === '-' || value === '*' || value === '/' || value === '%') {
+                if(screenValue.toString().length >= 1) {
+                    if(!isNaN(lastChar)) {
+                        if(progressCount < 1) {
+                            screenValue += value
+                            progressCount++;
+                        } else {
+                            screenValue = eval(screenValue).toString() + value
+                            progressCount = 1
+                            dotCount = 0;
+                        }
+                    } else {
+                        if(isNaN(lastChar)) {
+                            screenValue = screenValue.substring(0, screenValue.toString().length - 1)
+                            screenValue += value;
+                        }
+                        
+                    }
+                }
+            }
+            else if(value === '.') {
+                if(screenValue.toString().length >= 1) {
+                    if(!isNaN(lastChar)) {
+                        if(dotCount < 1) {
+                            screenValue += value
+                            progressCount++;
+                            dotCount++;
+                        }
+                    }
+                }
+            }
+            else if(value === 'C') {
+                screenValue = '0';
+            } else {
+                return false
+            }
+        }
+        progress = screenValue
+    }
+    function renderCalculatorScreen(value) {
+        screen.value = value
+    }
+    
+    
+})
